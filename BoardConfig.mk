74f9e35cbf0e99f841feabc4d03b19d805724e6e
Alexander Koskovich <zvnexus@outlook.com>
2020-10-28 14:22:44 +0530

    mido: Set TARGET_USES_HARDWARE_KEYS.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -142,6 +142,9 @@ DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_mido
 TARGET_RECOVERY_DEVICE_MODULES := libinit_mido
 
+# Keylayouts
+TARGET_USES_HARDWARE_KEYS := true
+
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 
