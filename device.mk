c2bff9cd37c371042d1253ddb83a5e1f78584376
Gagan Malvi <gagan@malvi.ml>
2020-11-19 11:36:56 +0530

    mido: Move overlays to configs/overlays
    Signed-off-by: Gagan Malvi <gagan@malvi.ml>

--- device.mk
+++ device.mk
@@ -19,8 +19,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pa
+    $(LOCAL_PATH)/configs/overlays/overlay \
+    $(LOCAL_PATH)/configs/overlays/overlay-pa
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
