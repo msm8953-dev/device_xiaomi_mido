32e3d1ddabc2a9b705d4b8d28ebdb845a461c9fc
Gagan Malvi <gagan@malvi.ml>
2020-11-29 23:01:41 +0530

    mido: Remove privapp-permissions-qti whitelist from device-tree
    * Already defined in vendor/pa
    
    Signed-off-by: Gagan Malvi <gagan@malvi.ml>

--- device.mk
+++ device.mk
@@ -377,10 +377,6 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libprotobuf-cpp-full-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libprotobuf-cpp-lite-v29.so
 
-# Whitelisted apps
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
-
 # WLAN
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service-lazy \
