bcf779cdef1eaa8d2099e3e764c148190d81607b
Gagan Malvi <gagan@malvi.ml>
2020-12-01 14:45:03 +0530

    mido: Build basic USB HAL.
    Signed-off-by: Gagan Malvi <gagan@malvi.ml>

--- device.mk
+++ device.mk
@@ -137,7 +137,6 @@ PRODUCT_PACKAGES += \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     libbtconfigstore \
     libbluetooth_qti \
-    vendor.qti.hardware.bluetooth_audio@2.0 \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
@@ -367,6 +366,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
+# USB
+PRODUCT_PACKAGES := \
+    android.hardware.usb@1.0-service.basic
 
 # VNDK
 PRODUCT_PACKAGES += \
